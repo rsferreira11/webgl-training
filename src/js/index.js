e0b127d5033dca359b1bc4ca656c6d493138d795
Rafael Ferreira <rsferreira11@gmail.com>
2018-10-31 19:02:50 +0200

    Review the first shader and use pixels instead of clipspace

--- src/js/index.js
+++ src/js/index.js
@@ -4,68 +4,91 @@ import vertexShaderSource from '../shaders/baseShader.vert';
 import { createCanvas } from './utils/createCanvas';
 
 function createShader(gl, type, source) {
-  var shader = gl.createShader(type);
+  const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
 
   if (success) {
     return shader;
   }
- 
+
   gl.deleteShader(shader);
+  return null;
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
-  var program = gl.createProgram();
+  const program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
-  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (success) {
     return program;
   }
- 
+
   gl.deleteProgram(program);
+  return null;
 }
 
 export const executeShader = gl => {
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-  
-  var program = createProgram(gl, vertexShader, fragmentShader);
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  var positionBuffer = gl.createBuffer();
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+
+  const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  
-  var positions = [
-   -1, -1,
-    0,  0,
-    1, -1,
+
+const positions = [
+    0, 360,
+    640, 720,
+    0, 720,
+    0, 0,
+    1280, 720,
+    1280, 0,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  var vao = gl.createVertexArray();
+
+  // Create a vertex array object (attribute state)
+  const vao = gl.createVertexArray();
+
+  // and make it the one we're currently working with
   gl.bindVertexArray(vao);
+
+  // Turn on the attribute
   gl.enableVertexAttribArray(positionAttributeLocation);
-  
-  var size = 2;          // 2 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-  
-  // webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+  const size = 2;          // 2 components per iteration
+  const type = gl.FLOAT;   // the data is 32bit floats
+  const normalize = false; // don't normalize the data
+  const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const pointerOffset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, pointerOffset);
+
+  // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  
+
   // Clear the canvas
   gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
-  
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 3;
+
+  // Bind the attribute/buffer set we want.
+  gl.bindVertexArray(vao);
+
+  // Pass in the canvas resolution so we can convert from
+  // pixels to clipspace in the shader
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+  const primitiveType = gl.TRIANGLES;
+  const offset = 0;
+  const count = 6;
   gl.drawArrays(primitiveType, offset, count);
 }
 
